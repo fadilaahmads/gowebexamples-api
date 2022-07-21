42bc13bf0aa7e44d8f47ee857389161a75ae04a5
fadilaahmads <fadilahmad59@gmail.com>
2022-07-21 19:23:18 +0700

    Hello World

--- main.go
+++ main.go
@@ -1,5 +1,16 @@
 package main
 
-func main() {
+import (
+	"fmt"
+	"net/http"
+)
 
+func main() {
+	// Registering a Request Handler
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprintf(w, "Hello, you've requested this %s path", r.URL.Path)
+	})
+
+	// Accept connection
+	http.ListenAndServe(":80", nil)
 }
