1860d7faa8aba3e9e8921bd667ef343f3d685bcd
fadilaahmads <fadilahmad59@gmail.com>
2022-07-21 20:54:07 +0700

    Add http method and create separate handler function

--- main.go
+++ main.go
@@ -7,22 +7,26 @@ import (
 	"github.com/gorilla/mux"
 )
 
+func Homepage(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Welcome to My Bookstore!")
+}
+
+func GetBooks(w http.ResponseWriter, r *http.Request) {
+	vars := mux.Vars(r)
+	// Segmenting Request
+	title := vars["title"]
+	page := vars["page"]
+
+	fmt.Fprintf(w, "You requested book: %s on page %s\n", title, page)
+}
+
 func main() {
 	// Create New Router
 	r := mux.NewRouter()
 
-	r.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		fmt.Fprintf(w, "Welcome to My Bookstore!")
-	})
+	r.HandleFunc("/", Homepage).Methods("GET")
 
-	r.HandleFunc("/books/{title}/page/{page}", func(w http.ResponseWriter, r *http.Request) {
-		vars := mux.Vars(r)
-		// Segmenting Request
-		title := vars["title"]
-		page := vars["page"]
-
-		fmt.Fprintf(w, "You requested book: %s on page %s\n", title, page)
-	})
+	r.HandleFunc("/books/{title}/page/{page}", GetBooks).Methods("POST")
 
 	http.ListenAndServe(":80", r)
 }
