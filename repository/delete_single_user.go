49f1fc681ea4bf88d3316cff74134290d75631db
fadilaahmads <fadilahmad59@gmail.com>
2022-07-22 20:03:03 +0700

    Create Delete single user

---
+++ repository/delete_single_user.go
@@ -0,0 +1,15 @@
+package repository
+
+import "log"
+
+func DeleteSingleUser() {
+	db, err := NewDB().Begin()
+	if err != nil {
+		log.Fatal(err)
+	}
+	// Delete user
+	if _, err := db.Exec(`DELETE FROM users WHERE id = ?`, 1); err != nil {
+		log.Fatal(err)
+	}
+
+}
