fd9bf87ed03fe6606921b357c32a4051fb586026
fadilaahmads <fadilahmad59@gmail.com>
2022-07-22 20:37:05 +0700

    Change sql prepared statement parameter

--- repository/delete_single_user.go
+++ repository/delete_single_user.go
@@ -2,13 +2,13 @@ package repository
 
 import "log"
 
-func DeleteSingleUser() {
+func DeleteSingleUser(id string) {
 	db, err := NewDB().Begin()
 	if err != nil {
 		log.Fatal(err)
 	}
 	// Delete user
-	if _, err := db.Exec(`DELETE FROM users WHERE id = ?`, 1); err != nil {
+	if _, err := db.Exec(`DELETE FROM users WHERE id = ?`, id); err != nil {
 		log.Fatal(err)
 	}
 
