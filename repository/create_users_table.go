6139795ad50fbb98b5885d8c203aab932ec4fdb3
fadilaahmads <fadilahmad59@gmail.com>
2022-07-22 19:21:55 +0700

    Add func for Create users table

---
+++ repository/create_users_table.go
@@ -0,0 +1,26 @@
+package repository
+
+import (
+	"fmt"
+	"log"
+)
+
+func CreateUsersTable() {
+	db, err := NewDB().Begin()
+	if err != nil {
+		log.Fatal(err)
+	}
+	query := `
+            CREATE TABLE users (
+                id INT AUTO_INCREMENT,
+                username TEXT NOT NULL,
+                password TEXT NOT NULL,
+                created_at DATETIME,
+                PRIMARY KEY (id)
+            );`
+
+	if _, err := db.Exec(query); err != nil {
+		log.Fatal(err)
+	}
+	fmt.Println("Table has been created!")
+}
