113b2babd3e4510d1def0bae2ead18d391b70060
fadilaahmads <fadilahmad59@gmail.com>
2022-07-22 17:08:47 +0700

    Create New Database connection

--- database/database.go
+++ database/database.go
@@ -7,6 +7,28 @@ import (
 	"time"
 )
 
+func NewDB() *sql.DB {
+	var (
+		host     = "127.0.0.1"
+		port     = "3306"
+		username = "root"
+		password = ""
+		dbname   = "gowebexamples"
+	)
+
+	psqlInfo := fmt.Sprintf("host= %s port= %s user= %s "+"password= %s dbname= %s", host, port, username, password, dbname)
+
+	db, err := sql.Open("mysql", psqlInfo)
+	if err != nil {
+		log.Fatal(err)
+	}
+	if err := db.Ping(); err != nil {
+		log.Fatal()
+	}
+
+	return db
+}
+
 func DB() {
 
 	// Configure the database connection (always check errors)
