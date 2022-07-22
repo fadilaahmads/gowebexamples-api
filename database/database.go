cdf451630f5b136f1a5536282cad1ec5049e5482
fadilaahmads <fadilahmad59@gmail.com>
2022-07-22 11:11:52 +0700

    Change function name, move file into separate package

---
+++ database/database.go
@@ -0,0 +1,56 @@
+package database
+
+import (
+	"database/sql"
+	"fmt"
+	"log"
+	"time"
+)
+
+func DB() {
+
+	// Configure the database connection (always check errors)
+	db, err := sql.Open("mysql", "username:password@(127.0.0.1:3306)/dbname?parseTime=true")
+
+	if err != nil {
+		log.Fatal(err)
+	}
+	// Initialize the first connection to the database, to see if everything works correctly.
+	if err := db.Ping(); err != nil {
+		log.Fatal(err)
+	}
+
+	{ // Create a new table
+		query := `
+            CREATE TABLE users (
+                id INT AUTO_INCREMENT,
+                username TEXT NOT NULL,
+                password TEXT NOT NULL,
+                created_at DATETIME,
+                PRIMARY KEY (id)
+            );`
+
+		if _, err := db.Exec(query); err != nil {
+			log.Fatal(err)
+		}
+	}
+
+	{
+		username := "John Doe"
+		password := "secret"
+		createdAt := time.Now()
+
+		// Inserts our data into the users table and returns with the result and a possible error.
+		// The result contains information about the last inserted id (which was auto-generated for us) and the count of rows this query affected.
+		result, err := db.Exec(`INSERT INTO users (username, password, created_at) VALUES (?, ?, ?)`, username, password, createdAt)
+		if err != nil {
+			log.Fatal(err)
+		}
+		id, err := result.LastInsertId()
+		if err != nil {
+			log.Fatal(err)
+		}
+		fmt.Println(id)
+	}
+
+}
