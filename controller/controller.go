912ff40aeb46fd860cc88888bcaac8605482d7af
fadilaahmads <fadilahmad59@gmail.com>
2022-07-22 19:20:39 +0700

    Create controlller file for route handler

---
+++ controller/controller.go
@@ -0,0 +1,25 @@
+package controller
+
+import (
+	"fmt"
+	"net/http"
+	"web-api/repository"
+
+	"github.com/gorilla/mux"
+)
+
+func CreateUserTable(w http.ResponseWriter, r *http.Request) {
+	repository.CreateUsersTable()
+}
+func Homepage(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Welcome to My Bookstore!")
+}
+
+func GetBooks(w http.ResponseWriter, r *http.Request) {
+	vars := mux.Vars(r)
+	// Segmenting Request
+	title := vars["title"]
+	page := vars["page"]
+
+	fmt.Fprintf(w, "You requested book: %s on page %s\n", title, page)
+}
