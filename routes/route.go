2cd80edd79942d2b1dd8ecfa18921d26b7d0bf00
fadilaahmads <fadilahmad59@gmail.com>
2022-07-23 22:12:11 +0700

    Add route for get all user and add user

--- routes/route.go
+++ routes/route.go
@@ -9,7 +9,8 @@ import (
 func Routes(r *mux.Router) {
 	r.HandleFunc("/table/create/user", controller.CreateUserTable)
 	r.HandleFunc("/user/delete/{id}", controller.DeleteSingleUser)
-
+	r.HandleFunc("/user/add/", controller.InsertNewUser)
+	r.HandleFunc("/user/get/all", controller.GetAllUser)
 	r.HandleFunc("/", controller.Homepage).Methods("GET")
 
 	r.HandleFunc("/books/{title}/page/{page}", controller.GetBooks).Methods("GET")
